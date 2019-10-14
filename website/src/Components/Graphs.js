137bedfa03dfee033f8a049fc6695a0f4afe201c
Kattenelvis <officialkattenelvis@gmail.com>
2019-10-14 18:44:39 +0200

    d

--- website/src/Components/Graphs.js
+++ website/src/Components/Graphs.js
@@ -9,7 +9,7 @@ export default class Graphs extends Component {
 				{
 					name: 'Alla',
 					pieces: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-				},
+				} /*,
 				{
 					name: 'Test',
 					pieces: [null]
@@ -17,7 +17,7 @@ export default class Graphs extends Component {
 				{
 					name: 'Test',
 					pieces: []
-				}
+				}*/
 			]
 		})
 	}
